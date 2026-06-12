12beff7f2eceb5035a462895b72f1b63d9c7c40b
xender22 <niccadrian@gmail.com>
2026-06-12 03:18:34 +0300

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public void SortColors(int[] nums) 
+    {
+        var colors = new int[] {0, 0, 0};
+
+        foreach (int num in nums) 
+        {
+            colors[num] += 1;
+        }
+
+        Console.WriteLine(string.Join(", ", colors));
+
+        var clrIdx = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            while (colors[clrIdx] <= 0)
+                clrIdx++;
+
+            nums[i] = clrIdx;
+            colors[clrIdx]--;
+            
+        }
+    }
+}
